576aed2b4d312e58f25063c17caac058676e422b
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-01 22:09:41 +0900

    portfolio update.

--- main.js
+++ main.js
@@ -3,18 +3,34 @@ const manga=[{title:"ハイキュー!!",url:"https://www.shonenjump.com/j/rensai
     {title:"ブラッククローバー",url:"https://www.shonenjump.com/j/rensai/bclover.html"},
     {title:"ワールドトリガー",url:"https://www.shonenjump.com/j/rensai/world.html"},
     {title:"Dr.STONE",url:"https://www.shonenjump.com/j/rensai/drstone.html"},
+    {title:"ONE PIECE",url:"https://www.shonenjump.com/j/rensai/onepiece.html"},
+    {title:"僕のヒーローアカデミア",url:"https://www.shonenjump.com/j/rensai/myhero.html"},
+    {title:"暗殺教室",url:"https://www.shonenjump.com/j/rensai/ansatsu.html"},
+    {title:"NARUTO-ナルト-",url:"https://www.shonenjump.com/j/rensai/naruto.html"},
+    {title:"ダイヤのA",url:"https://diaace.com/"},
 ]
 
 const scene=["https://livedoor.blogimg.jp/anigei-mangabox/imgs/4/d/4dd6bd91-s.jpg",
     "https://manga-comic-netabare.com/wp-content/uploads/2020/08/CnadM5kWAAAPjjy-768x689.jpg",
+    "https://newsinfomation.net/wp-content/uploads/2021/07/8-11.jpg",
     "https://blogimg.goo.ne.jp/user_image/6d/da/4375040fc5a40054eb69fea787ed1359.jpg?1597877727",
     "https://renote.jp/files/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOXRxRXc9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--bd0eac625e4c105ef2b94d1c232923b239f9a587/186%5B2138%5D.jpg",
+    "https://ritaremake-jinseikakerubokuwa.com/wp-content/uploads/2020/03/%E3%82%AD%E3%83%A3%E3%83%97%E3%83%81%E3%83%A3796-768x576.png",
+    "https://manga-netabare-kanso.com/wp-content/uploads/2018/04/%E3%83%8F%E3%82%A4%E3%82%AD%E3%83%A5%E3%83%BC298%E8%A9%B1%E6%9C%88%E5%B3%B6%EF%BC%88%E3%83%84%E3%83%83%E3%82%AD%E3%83%BC%EF%BC%89.png",
     "https://s3-ap-northeast-1.amazonaws.com/cdn.bibi-star.jp/production/imgs/images/000/631/121/original.?1618474647",
     "https://manga-comic-netabare.com/wp-content/uploads/2020/01/20171006093355.jpg",
     "https://1.bp.blogspot.com/-8XKF_u8Q0mE/Xs5gJlBo_UI/AAAAAAAAFGk/0h5sD9APGXgsTDRZuI7uemQARZqW_Ii6wCPcBGAYYCw/s0/kokusen-min.jpg",
     "https://i0.wp.com/mannga-brothers.com/wp-content/uploads/2019/03/IMG_2773.jpg?resize=1280%2C720&ssl=1",
     "https://manga-netabare-kanso.com/wp-content/uploads/2021/01/drstone_senku_02-768x876.jpg",
-    "https://s3-ap-northeast-1.amazonaws.com/cdn.bibi-star.jp/production/imgs/images/000/587/296/original.jpg?1608533541"
+    "https://s3-ap-northeast-1.amazonaws.com/cdn.bibi-star.jp/production/imgs/images/000/587/296/original.jpg?1608533541",
+    "https://cdn-ak.f.st-hatena.com/images/fotolife/d/dokusyo_geek_ki/20181128/20181128022834.png",
+    "https://cdn-ak.f.st-hatena.com/images/fotolife/d/dokusyo_geek_ki/20181203/20181203154632.png",
+    "https://renote.jp/files/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaEgzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f046414a51a690aa0d184569d9ecc1add7963488/8.jpg",
+    "https://renote.jp/files/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaHIzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--8de349bb8bb8c037e77836fc1d7cfbdda6da176d/21900085154cd16fd89.jpg",
+    "https://assets.st-note.com/production/uploads/images/9796525/picture_pc_656a28370f766dfb922a236b4129a253.jpg?width=800",
+    "https://animemiru.jp/wp-content/uploads/2018/08/naruto-gai06.jpg",
+    "https://www.cmoa.jp/cc/template/common/titleinfo/quotation/data/39221/images/39221_04.png",
+    "https://www.cmoa.jp/cc/template/common/titleinfo/quotation/data/39221/images/39221_02.png"
 ]
 
 const divinationResult=[{result:"喜び",url:"./image/happy.webp"},
@@ -27,6 +43,9 @@ const divinationResult=[{result:"喜び",url:"./image/happy.webp"},
     {result:"信頼",url:"./image/trust.webp"},
 ]
 
+const intro=document.getElementById("intro")
+const famousScene=document.getElementById("famous-scene")
+const omake=document.getElementById("omake")
 const mangaList=document.getElementById("mangalist")
 const favoriteScene=document.getElementById("favorite-scene")
 const zeroButton=document.getElementById("zero")
@@ -38,6 +57,36 @@ const fiveButton=document.getElementById("five")
 const sixButton=document.getElementById("six")
 const sevenButton=document.getElementById("seven")
 const divinationId=document.getElementById("divination")
+
+//マウスカーソルがHTML要素に乗ったとき
+intro.addEventListener('mouseover', function() {
+    intro.style.color="blue"
+});
+
+//マウスカーソルがHTML要素から離れたとき
+intro.addEventListener('mouseout', function() {
+    intro.style.color=null
+});
+
+//マウスカーソルがHTML要素に乗ったとき
+famousScene.addEventListener('mouseover', function() {
+    famousScene.style.color="blue"
+});
+
+//マウスカーソルがHTML要素から離れたとき
+famousScene.addEventListener('mouseout', function() {
+    famousScene.style.color=null
+});
+
+//マウスカーソルがHTML要素に乗ったとき
+omake.addEventListener('mouseover', function() {
+    omake.style.color="blue"
+});
+
+//マウスカーソルがHTML要素から離れたとき
+omake.addEventListener('mouseout', function() {
+    omake.style.color=null
+});
 
 //mangaの要素全てを好きな漫画のところに表示させる関数
 const addManga=function(){
